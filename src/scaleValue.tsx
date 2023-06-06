8f18c7b637adae9fbd0d41a3380410078fa0b5fb
Fabian Althaus [el-j] <el-j@users.noreply.github.com>
2023-06-06 15:53:17 +0200

    Update scaleValue.tsx

--- src/scaleValue.tsx
+++ src/scaleValue.tsx
@@ -1,3 +1,4 @@
+//inspired by https://stackoverflow.com/a/23202637
 export function scaleValue(number: number, inMin: number, inMax: number, outMin: number, outMax: number) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
