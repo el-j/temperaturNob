f3a4f973928fafc494e5ea524d6abb59bb935f80
el-j <bluearc.in.sky@gmail.com>
2023-06-06 17:20:07 +0200

    add another styled version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TempNob from './TempNob';
+import TempNobStyled from './TempNobStyled';
 import { Interface } from './Interface';
 
 function App() {
@@ -9,9 +10,16 @@ function App() {
   const [value, setValue] = React.useState(20)
 
   return (
-    <div style={{ display: "grid" }}>
+    <div style={{ display: "grid",justifyContent: "center"  }}>
+              <h5>simple version:</h5>
       <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+
         <TempNob min={minValue} max={maxValue} value={value}></TempNob>
+      </div>
+      <h5>other styled version:</h5>
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+        
+        <TempNobStyled min={minValue} max={maxValue} value={value}></TempNobStyled>
       </div>
       <div style={{ display: "flex", flexDirection: "row", flexGrow: 1, justifyContent: "space-around", marginTop: "16px" }}>
         <Interface name={"min"} value={minValue} setValue={setMinValue} />
---
+++ src/TempNobStyled.tsx
@@ -0,0 +1,95 @@
+import { scaleValue } from "./scaleValue";
+
+interface ITempNob {
+    min: number
+    max: number
+    value: number
+}
+
+const TempNobStyled = ({ min, max, value }: ITempNob) => {
+    const showValue = scaleValue(value, min, max, -141, 141)
+    const nobColor = "black"
+    const bgColor = showValue > 50 ? `rgba(255,100,100,${scaleValue(showValue, -141, 141, 0, 0.3)})` : showValue < -50 ? `rgba(0,0,200,${scaleValue(showValue, 141, -141, 0, 0.3)})` : `rgba(240,240,240,${scaleValue(showValue, -141, 141, 0.4, 1)})`
+    return <div style={{
+        height: 200,
+        width: 200,
+        flexFlow: "column",
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center"
+    }}>
+        <div style={{
+            height: 100,
+            width: 100,
+            borderRadius: "100%",
+            position: "relative",
+            transition: "all 1s ease 0s"
+        }}
+        >
+            <div style={{
+                background: bgColor,
+                height: 100,
+                width: 100,
+                borderRadius: "100%",
+                position: "absolute",
+                transition: "all 1s ease 0s"
+            }}
+            >
+                <div style={{
+                    width: "2px",
+                    height: "100px",
+                    position: "relative",
+                    transform: `rotate(${showValue}deg)`,
+                    transformOrigin: "center",
+                    margin: "auto",
+                    transition: "all 1s ease 0s"
+                }}>
+                    <div style={{
+                        transition: "all 1s ease 0s",
+                        width: 0,
+                        height: 0,
+                        borderStyle: "solid",
+                        borderWidth: "0 2.5px 20px 2.5px",
+                        borderColor: `transparent transparent ${nobColor} transparent`,
+                        position: "absolute",
+                        top: 5
+                    }}>
+                    </div>
+                </div>
+                <div style={{
+                    width: 0,
+                    height: 0,
+                    position: "absolute",
+                    bottom: "0",
+                    transform: "rotate(40deg)",
+                    borderStyle: "solid",
+                    borderWidth: "0 2.5px 10px 2.5px",
+                    borderColor: `transparent transparent ${nobColor} transparent`,
+                    left: "14px"
+                }}></div>
+                <div style={{
+                    width: 0,
+                    height: 0,
+                    position: "absolute",
+                    bottom: "0",
+                    transform: "rotate(-40deg)",
+                    borderStyle: "solid",
+                    borderWidth: "0 2.5px 10px 2.5px",
+                    borderColor: `transparent transparent ${nobColor} transparent`,
+                    right: "14px"
+                }}></div>
+            </div>
+        </div>
+        <div style={{
+            height: 50,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+        }}>{value} °C
+        </div>
+    </div>
+}
+
+
+
+export default TempNobStyled;
